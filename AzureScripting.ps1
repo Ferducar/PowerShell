45508e7f47dc23fee685d4d3424c30bff3bf2b13
CarlosFer <ferducarone@gmail.com>
2022-08-26 23:28:33 -0500

    adding tester branc and tf file

--- AzureScripting.ps1
+++ AzureScripting.ps1
@@ -10,20 +10,25 @@ az login
 
 GIT BASIC COMMANDS
 git init - para inicializar el proyecto nuevo o alguno existente.
-git add <file> - para pasar los archivos del working directory al staging area / tambien se usa para actualizar tus archivos activos
+git add <file> or <files> - para pasar los archivos del working directory al staging area / tambien se usa para actualizar tus archivos activos
 git status - el estado de tus archivos/proyecto
 git commit - para pasarlos del staging area al repository, ya sea local o remoto
+git commit -m "add commit comment"
 git push - para subirlo a un repositorio remoto como github
 git pull - para traer los cambios del proyecto remoto al local
 git clone - copia del repo remote al local
 git rm --cached <file> to unstage
 git log 
 git restore <file>
-
+git branch - para crear nuevos caminos, nuevas versions
+git checkout <branch> - to change branches
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
- 
+
+GIT COMMENTS
+Git will ignore empty folders
+
 GITHUB STATES
 Working directory > git add > staging area > git commit > repository
 
