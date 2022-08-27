06331447f211404e1b5ab3b300b5dcf96fd6af84
CarlosFer <ferducarone@gmail.com>
2022-08-26 23:03:07 -0500

    InitialCommit

---
+++ AzureScripting.ps1
@@ -0,0 +1,33 @@
+Connect-AzAccount
+az login
+
+
+
+
+
+
+<#
+
+GIT BASIC COMMANDS
+git init - para inicializar el proyecto nuevo o alguno existente.
+git add <file> - para pasar los archivos del working directory al staging area / tambien se usa para actualizar tus archivos activos
+git status - el estado de tus archivos/proyecto
+git commit - para pasarlos del staging area al repository, ya sea local o remoto
+git push - para subirlo a un repositorio remoto como github
+git pull - para traer los cambios del proyecto remoto al local
+git clone - copia del repo remote al local
+use "git rm --cached <file>..." to unstage
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+GITHUB STATES
+Working directory > git add > staging area > git commit > repository
+
+#>
+
+
+
+
+
+
