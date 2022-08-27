fd47c80ae00487d2d5fc23eac70117ddb21cc9ce
CarlosFer <ferducarone@gmail.com>
2022-08-27 00:09:18 -0500

    damn

--- AzureScripting.ps1
+++ AzureScripting.ps1
@@ -28,7 +28,7 @@ git remote add origin https://github.com/Ferducar/PowerShell - Para decirle dond
 git remote set-url origin https://github.com/Ferducar/PowerShell - to modify our origin
 git push -u origin master - maybe its only done once? not sure tho
 git push --set-upstream origin tester - to add your local branch to your remote repo
-
+git clone  <url> - this is to download our repo to local
 
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
