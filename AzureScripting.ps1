c888555f60c0c39a908364f94c33cc41c31faee8
CarlosFer <ferducarone@gmail.com>
2022-08-27 10:32:23 -0500

    master changesv1

--- AzureScripting.ps1
+++ AzureScripting.ps1
@@ -15,6 +15,9 @@ git status - el estado de tus archivos/proyecto
 git commit - para pasarlos del staging area al repository, ya sea local o remoto
 git commit -m "add commit comment"
 git push - para subirlo a un repositorio remoto como github
+git push origin --delete <old-name-branch>	delete old branch
+git push -u origin <branch-name>		push local branch for the first time
+git push --set-upstream origin tester - to add your local branch to your remote repo and his code/files
 git pull - para traer los cambios del proyecto remoto al local
 git clone - copia del repo remote al local
 git rm --cached <file> to unstage
@@ -26,12 +29,17 @@ git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 git remote add origin https://github.com/Ferducar/PowerShell - Para decirle donde guardaremos el codigo/proyecto
 git remote set-url origin https://github.com/Ferducar/PowerShell - to modify our origin
-git push -u origin master - maybe its only done once? not sure tho
-git push --set-upstream origin tester - to add your local branch to your remote repo and his code/files
+
+
 git clone  <url> - this is to download our repo to local
 git log --oneline --decorate --all --graph
 git pull origin my_remote_branch - to download changes
 git remote show origin - to show my remote repo
+git log --oneline --decorate --all --graph
+git branch -r	list remote branches
+git branch -a	list remote and local branches
+git branch - m <new-name>	rename local head branch
+git branch -m <old-name> <new-name>		rename any local branch
 
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name
